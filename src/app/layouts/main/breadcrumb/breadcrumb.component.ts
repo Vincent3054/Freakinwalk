6e6701c039f4a73dd8a27a8a8d87d3358f04562e
CLG01\15960 <SkyChen@chailease.com.tw>
2024-04-25 14:55:47 +0800

    feat:完成header & fotter

--- src/app/layouts/main/breadcrumb/breadcrumb.component.ts
+++ src/app/layouts/main/breadcrumb/breadcrumb.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-breadcrumb',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './breadcrumb.component.html',
   styleUrl: './breadcrumb.component.scss'
 })
 export class BreadcrumbComponent {
+  breadcrumbs = [
+    { name: '首頁', url: '/' },
+    { name: '目錄', url: '/catalog' },
+    { name: '項目', url: '/catalog/item' }
+  ];
 
+  breadcrumbsExpanded = false;
+
+  toggleBreadcrumbs() {
+    this.breadcrumbsExpanded = !this.breadcrumbsExpanded;
+  }
 }
