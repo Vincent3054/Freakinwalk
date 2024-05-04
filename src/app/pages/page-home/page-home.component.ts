68e2f6b3dee90e269c06f43d99ae5a32371a7897
Vincent3054 <ok96305@gmail.com>
2024-05-05 02:31:35 +0800

    feat:首頁 part4 主視覺

--- src/app/pages/page-home/page-home.component.ts
+++ src/app/pages/page-home/page-home.component.ts
@@ -9,5 +9,26 @@ import { SplitmediaTextComponent } from './../../shared/compoents/splitmedia-tex
   styleUrl: './page-home.component.scss'
 })
 export class PageHomeComponent {
+  images = [
+    "../../../assets/images/visual3.png",
+    "../../../assets/images/visual1.png",
+    "../../../assets/images/visual2.png"
+  ];
+  currentIndex = 1; // 預設顯示 "視覺圖一"，即 index 1
 
+  changeImage(direction: string): void {
+    if (direction === 'next') {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    } else if (direction === 'prev') {
+      this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+    }
+    this.updateImageSrc();
+  }
+
+  updateImageSrc(): void {
+    const imageElement = document.querySelector('.visual-img') as HTMLImageElement;
+    if (imageElement) {
+      imageElement.src = this.images[this.currentIndex];
+    }
+  }
 }
